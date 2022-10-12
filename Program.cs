28d4c8062e6683f322186e12f914e1731dc007ef
rafimk <rafi.genius.cs@gmail.com>
2022-10-12 19:13:18 +0400

    Bug fix

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TFS_WorkItem_Issue_Fix;
 
 var notString = "";
 var token = "";
@@ -11,7 +14,7 @@ var token = "";
 using (HttpClient client = new HttpClient())
 {
     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic," Convert.ToBase64String(Encoding.ASCII.GetBytes($"{notString}:{token}")));
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{notString}:{token}")));
     
     using (HttpResponseMessage response = await client.GetAsync("http://tfs/tfs/TestCollection/supplychain/_apis/wit/workitems?ids=1&api-version=6.0"))
     {
@@ -29,7 +32,7 @@ var goodString = WebUtility.HtmlEncode(notString);
 using (HttpClient client = new HttpClient())
 {
     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic," Convert.ToBase64String(Encoding.ASCII.GetBytes($"{notString}:{token}")));
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{notString}:{token}")));
     
     List<TFSContract> patchDocument = new List<TFSContract>();
     patchDocument.Add(new TFSContract { op = "add", path = "/fields/System.Description", value = goodString });
